2f41b5fb81b7d96eaa4e3417c3d852377b5874aa
amrali21 <amoormoneer@gmail.com>
2025-07-08 13:50:02 +0300

    add crud operations in frontend

---
+++ BlazorSampleApplication/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorSampleApplication.Models
+{
+    public class TodoItem
+    {
+        public TodoItem()
+        {
+            Id = new Guid();
+        }
+
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
