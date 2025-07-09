323e365d9b810122a83e5eb37ab963ccf09f044f
amrali21 <amoormoneer@gmail.com>
2025-07-09 17:03:31 +0300

    add required validation to title

--- BlazorSampleApplication/DbModels/Todo.cs
+++ BlazorSampleApplication/DbModels/Todo.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorSampleApplication.DbModels;
 
 public partial class Todo
 {
-    public int Id { get; set; } 
-
+    public int Id { get; set; }
+    
+    [Required]
     public string? Title { get; set; }
 
     public string? Description { get; set; }
